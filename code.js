64091b946d427f03665b5c299b65620b46598b65
Hannes Levéque <hannes.leveque@learnet.se>
2020-09-07 12:12:05 +0200

    Newest update

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 //Realtid
 const timeP = document.querySelector("#realTime");
-
+const chatT = document.querySelector("#chatTime");
 // skapar interval med uppdatering av tiden
 // varje minut
 function updateTime() {
@@ -21,5 +21,48 @@ function realTime() {
   var time = h + ":" + m;
   timeP.textContent = time;
 }
+//Väntar till ett element har "end" class och isåfall visas valmöjligheterna
+//och storleken på smsrutan ändras
+function onStartOB() {
+  var optionPick = document.querySelector(".option-picker");
+  var chatbox = document.querySelector(".iphone-chat");
+  var cp = document.querySelector("#cp");
+  if (optionPick.style.display == "none" && cp) {
+    addOB();
+  } else {
+    removeOB();
+  }
+}
+
+//Visar valen
+function addOB() {
+  var optionPick = document.querySelector(".option-picker");
+  var chatbox = document.querySelector(".iphone-chat");
+  optionPick.style.display = "block";
+  chatbox.style.height = "75%";
+}
+
+//Tar bort valen
+function removeOB() {
+  var optionPick = document.querySelector(".option-picker");
+  var chatbox = document.querySelector(".iphone-chat");
+  optionPick.style.display = "none";
+  chatbox.style.height = "97%";
+}
+
+//Tar bort checkpoint
+function removeCheckPoint(val) {
+  document.querySelector("#cp").removeAttribute("id");
+  removeOB();
+}
+
+var val1 = document.querySelector("#a");
+val1.addEventListener("click", removeCheckPoint);
+var val2 = document.querySelector("#b");
+val2.addEventListener("click", removeCheckPoint);
+var val3 = document.querySelector("#c");
+val3.addEventListener("click", removeCheckPoint);
+
 realTime();
 updateTime();
+onStartOB();
